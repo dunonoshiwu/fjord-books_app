3fe05b3f16aeb8b353d290fd915d93af60d82b85
duno <dunonoshiwu@gmail.com>
2021-07-22 22:13:32 +0900

    マイグレーションファイルのfollower_id, followed_idにnull false制約を追加しました

--- db/migrate/20210713031538_create_relationships.rb
+++ db/migrate/20210713031538_create_relationships.rb
@@ -3,8 +3,8 @@
 class CreateRelationships < ActiveRecord::Migration[6.1]
   def change
     create_table :relationships do |t|
-      t.bigint :follower_id
-      t.bigint :followed_id
+      t.bigint :follower_id, null: false
+      t.bigint :followed_id, null: false
 
       t.timestamps
     end
